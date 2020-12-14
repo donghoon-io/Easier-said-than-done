c4dbafc66057c0a5e0154866d05ef8d4fa1d855e
Donghoon Shin <shin@donghoon.io>
2020-12-14 20:54:00 +0900

    고소

---
+++ Instell 10/고소.swift
@@ -0,0 +1,17 @@
+//
+//  고소.swift
+//  Instell 10
+//
+//  Created by Donghoon Shin on 2020/12/14.
+//
+
+import Foundation
+
+var goso = [
+    Ending(id: "1", gif: "goso_1.gif"),
+    Ending(id: "2", imageName: "goso_2.tiff", text: "그 이후, A와 담임선생님은 증거를 모으고 고소를 진행했다.\n이 사건을 공론화시켜 뉴스와 신문에 학교 이름이 나기도 했다."),
+    Ending(id: "3", gif: "goso_3.gif"),
+    Ending(id: "4", imageName: "goso_4.tiff", text: "그러나 A는 이내 자신의 선택을 후회하기 시작한다.\n처음에는 학년부장에게 심한 협박과 인신공격을 받았고,\n나중에는 학생들에게까지 손가락질을 받았다.\n담임선생님도 나중에는 지치셨는지 A의 상담 요청을 몇 번 거절하셨다."),
+    Ending(id: "5", gif: "goso_5.gif"),
+    Ending(id: "6", imageName: "goso_6.tiff", text: "시간은 흘러 그 해 입시가 끝났고, 대학에 갈 사람은 다 갔다.\nA는 반 년 동안 학교와의 싸움에 지쳤고, 원하던 대학에 가지 못했다.\n문득 ‘내가 뭘 위해 그렇게 했나’라는 생각이 들고 앞으로가 막막해진다")
+]
